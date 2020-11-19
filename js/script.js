69ad4323c17a692a81c022681af04a7230dac771
alessiogarifo92 <alessiogarifo92@gmail.com>
2020-11-19 10:17:45 +0100

    BONUS INSERITI

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@
 // un input permette all’utente di scrivere una “cosa da fare”;
 // un bottone permette di aggiungere quella cosa alla lista di cose da fare in pagina, come promemoria;
 // una volta che l’utente ha ipoteticamente svolto l’attività, grazie ad un bottone (X) può eliminare quella determinata cosa dalla lista.
+/////////////////////////////////////////////// BONUS////////////////////////////////////////////////
+// dò la possibilità anche di inserire la nuova “cosa da fare”, scritta nell’input anche con la pressione del tasto ‘enter’ da tastiera;
+// l’utente non può inserire più volte la stessa “cosa da fare”;
 
 // creo array vuoto dove inserire elementi che aggiungero via via come stringhe di addElement
 var app = new Vue ({
